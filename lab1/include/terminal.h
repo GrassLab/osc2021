124996841a208663ea311f36a8d1fc50fbefe7bf
Ken Lee <ken2812221@gmail.com>
2021-02-28 10:44:28 +0000

    Add lab1

---
+++ lab1/include/terminal.h
@@ -0,0 +1,14 @@
+#ifndef TERMINAL_H
+#define TERMINAL_H
+
+class Terminal {
+public:
+    Terminal();
+    void Run();
+private:
+    void Help();
+    void Hello();
+    void Reboot();
+};
+
+#endif
