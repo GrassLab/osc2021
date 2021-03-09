89bc6904b3269780a8b5d9df60a465e6ad927658
Ian Chen <ianre657@gmail.com>
2021-03-09 22:32:19 +0800

    🎉  init bootloader code from existing kernel

---
+++ bootloader/src/main.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+#include "uart.h"
+
+int main() {
+  uart_init();
+
+  uart_println("-------------------------------");
+  uart_println(" Operating System Capstone 2021");
+  uart_println("-------------------------------");
+
+  while (1) {
+    shellPrintPrompt();
+    shellInputLine();
+    shellProcessCommand();
+  }
+}
