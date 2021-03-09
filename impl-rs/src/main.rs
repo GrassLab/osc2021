4dfc350f7d36b8614a5d7644be392b38466cf2d0
Ian Chen <ianre657@gmail.com>
2021-03-09 22:29:33 +0800

    🎉  [rs] fix linkscript, able to boot
    update

--- impl-rs/src/main.rs
+++ impl-rs/src/main.rs
@@ -1,22 +1,22 @@
 #![no_std]
 #![no_main]
-#![feature(const_fn_fn_ptr_basics)]
-#![feature(format_args_nl)]
-#![feature(trait_alias)]
+#![feature(global_asm)]
 
 use core::panic::PanicInfo;
 
-mod boot;
 mod cpu;
 mod gpio;
 mod memory;
 mod uart;
+
+global_asm!(include_str!("boot.S"));
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+#[no_mangle]
 pub unsafe fn main() -> ! {
     uart::init_uart();
     loop {
