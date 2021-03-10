718364a7fd51d9a28c39d43349dd367f214c0f00
fatz <fatz.zhang@gmail.com>
2021-03-10 18:28:55 +0000

    kernel sender update

--- tty.py
+++ tty.py
@@ -1,4 +1,8 @@
-with open('/dev/pts/5', 'wb', buffering = 0) as tty,  open('./build/kernel8.img', 'rb') as f:
-    tty.write('s'.encode())
-    tty.write(f.read())
-    tty.write('end'.encode())
+import sys
+if len(sys.argv) > 1:
+    with open(sys.argv[1], 'wb', buffering = 0) as tty,  open('./build/kernel8.img', 'rb') as f:
+        tty.write('s'.encode())
+        tty.write(f.read())
+        tty.write('end'.encode())
+else:
+    print("Failed, please specify a device path")
