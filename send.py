d6d837d220109ae82b0b4a166d80103c13f14c42
yyWu <warren03wu@gmail.com>
2021-03-16 21:45:43 +0800

    Lab2 done. elective done

--- send.py
+++
@@ -1,9 +0,0 @@
-with open('/dev/pts/4', "wb", buffering = 0) as tty:
-    with open('./bootloader.img', "rb") as fp:
-        c = fp.read(1)
-        while c:
-            tty.write(c)
-            c = fp.read(1)
-
-fp.close()
-tty.close()
