dbfbc70e7afd66b03945f39333dcba0185cf0348
Yunyung <yungyung7654321@gmail.com>
2021-03-18 05:36:17 +0800

    feat: bootloader function
    Add bootloader folder, which allow us to transmit our actual kernel image
    through UART.

---
+++ bootloader/include/string.h
@@ -0,0 +1,13 @@
+#ifndef STRING_H
+#define STRING_H
+
+int  strcmp     ( char * s1, char * s2 );
+void strset     ( char * s1, int c, int size );
+int  strlen     ( char * s );
+void itoa       ( int x, char str[], int d);
+void ftoa       ( float n, char* res, int afterpoint ); 
+int atoi (const char * str);
+void reverse    ( char *s );
+
+
+#endif
