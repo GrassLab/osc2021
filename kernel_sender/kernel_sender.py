f213c1576585e511052dbf35c136460d768ffe87
kishow01 <kishow01@gmail.com>
2021-03-17 06:00:24 -0700

    fix

--- kernel_sender/kernel_sender.py
+++ kernel_sender/kernel_sender.py
@@ -3,14 +3,13 @@ from pwn import *
 from time import sleep
 
 kernel = open('./kernel8.img', 'rb').read()
-r = serialtube('/dev/ttyUSB0', convert_newlines=False)
 
-sleep(5)
-print(r.read())
-r.write(p32(len(kernel)))
-print(r.read())
-sleep(1)
-r.write(kernel)
-print(r.read())
-r.close()
+print("Start transmitting ...")
+
+with open('/dev/ttyUSB0', "wb", buffering = 0) as tty:
+    tty.write(p32(len(kernel)))
+    sleep(1)
+    tty.write(kernel)
+
+print("Finish!")
 
