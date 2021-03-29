85baf8bdb314a413c00ec051579a25fe60cf84d2
Ian Chen <ianre657@gmail.com>
2021-03-29 16:16:49 +0800

    🚧 interface for slab allocator

--- impl-c/include/mem.h
+++ impl-c/include/mem.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "list.h"
+#include <stdint.h>
 
 // Configuration of the Buddy system
 // Key conf:
@@ -34,25 +35,34 @@ typedef struct BuddyAllocater {
   Frame *frame_array;
 } BuddyAllocater;
 
-// // SlabAllocator
-// //    manage slabs with the same allocation size,
-// //    A slab is a frame allocated with small objects with same size
-// //      + SLAB_MAX_SLOTS: The number of pages available for a single slab
-// #define SLAB_MAX_SLOTS 256
-// typedef struct SlabAllocator {
-//   int unit_size; // size of the unit (in Bytes)
-//   uint8_t usage[SLAB_MAX_SLOTS >> 3];
+// SlabAllocator
+//    manage slabs with the same allocation size,
+//    A slab is a frame allocated with small objects with same size
+//      + @SLAB_MAX_SLOTS: The number of pages available for a single slab
+//        -> Frame is 4kb by architecture.
+//          CortexA53 is 16 bytes aligned, so the here 16 bytes is set to be
+//          the minimum size available for objects in slab.
+//        -> Therefore, the maximum slots in slab is 4096/16 = 256
+#define SLAB_MAX_SLOTS 256
 
-//   // Buffer indicate the current frames that would be use for obejct
-//   allocation
-//   // once buffer is fill, it would be added to fulled_list.
-//   // Frames in fulled_list would be move to partial_list once an object is
-//   freed struct Frame *buffer; list_head_t partial_list; list_head_t
-//   fulled_list;
-// } SlabAllocator;
+//  The size of slab range from 16(2^4) to 512(2^9) bytes
+#define SLAB_OBJ_MIN_SIZE_EXP 4
+#define SLAB_OBJ_MAX_SIZE_EXP 9
+#define SLAB_NUM_SLAB_SIZES 6
+
+typedef struct SlabAllocator {
+  int unit_size; // size of the unit (in Bytes)
+  uint8_t usage[SLAB_MAX_SLOTS];
+
+  struct Frame *cur_frame;  // The current frame be used for obejct allocation
+  list_head_t partial_list; // Frames in fulled_list would be move to
+                            //  partial_list once an object is freed
+  list_head_t fulled_list;  // Once cur_frame is full, it would be dispatch to
+                            //  fulled_list for storage.
+} SlabAllocator;
 
 typedef struct AllocationManager {
-  // SlabAllocator obj_allocators[4]; // 16, 32, 48, 96
+  SlabAllocator obj_allocator_list[SLAB_NUM_SLAB_SIZES];
   BuddyAllocater *frame_allocator;
 } AllocationManager;
 
