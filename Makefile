c38a7d9e9d827ac17a6aa9d2ba64b8fa71396d62
Brothre23 <daveyu824@gmail.com>
2021-04-04 09:53:11 +0800

    LAB-04 initial

---
+++ Makefile
@@ -0,0 +1,35 @@
+SRCS = $(wildcard ./src/*.c)
+OBJS = $(addprefix ./build/, $(notdir $(SRCS:.c=.o)))
+
+INCLUDE_DIR = include
+SRC_DIR = src
+OBJ_DIR = build
+
+CFLAGS = -Wall -O2 -ffreestanding -nostdlib
+CC = aarch64-linux-gnu-gcc
+LINKER = aarch64-linux-gnu-ld
+OBJ_CPY = aarch64-linux-gnu-objcopy
+
+.PHONY: clean run deploy
+
+all: kernel8.img
+
+start.o: start.S
+	$(CC) $(CFLAGS) -c start.S -o start.o
+
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
+	$(CC) $(CFLAGS) -I$(INCLUDE_DIR) -c $< -o $@
+
+kernel8.img: start.o $(OBJS)
+	$(LINKER) -nostdlib start.o $(OBJS) -T link.ld -o kernel8.elf
+	$(OBJ_CPY) -O binary kernel8.elf kernel8.img
+
+clean:
+	rm kernel8.elf kernel8.img start.o build/*.o >/dev/null 2>/dev/null || true
+
+run: all
+	qemu-system-aarch64 -M raspi3 -kernel kernel8.img -display none -serial null -serial stdio
+
+deploy: all
+	cp ./kernel8.img /run/media/brothre23/4DFF-0A36/
+	sudo eject /dev/sdc
---
+++ include/list.h
@@ -0,0 +1,61 @@
+
+#ifndef LIST_H
+#define LIST_H
+
+typedef struct list_head
+{
+    struct list_head *prev, *next;
+} list_head_t;
+
+static inline void list_init_head(list_head_t *list)
+{
+    list->prev = list->next = list;
+}
+
+static inline void __list_add(list_head_t *new_, list_head_t *prev, list_head_t *next)
+{
+    next->prev = new_;
+    new_->next = next;
+    new_->prev = prev;
+    prev->next = new_;
+}
+
+static inline void list_add(list_head_t *new_, list_head_t *head)
+{
+    __list_add(new_, head, head->next);
+}
+
+static inline void list_add_tail(list_head_t *new_, list_head_t *head)
+{
+    __list_add(new_, head->prev, head);
+}
+
+// concatenate list
+static inline void list_cat(list_head_t *new_h, list_head_t *new_t, list_head_t *head)
+{
+    new_h->prev = head;
+    new_t->next = head->next;
+    head->next->prev = new_t;
+    head->next = new_h;
+}
+
+static inline void list_cat_tail(list_head_t *new_h, list_head_t *new_t, list_head_t *head)
+{
+    new_h->prev = head->prev;
+    head->prev->next = new_h;
+    head->prev = new_t;
+    new_t->next = head;
+}
+
+static inline void list_crop(list_head_t *list_h, list_head_t *list_t)
+{
+    list_h->prev->next = list_t->next;
+    list_t->next->prev = list_h->prev;
+}
+
+static inline int list_empty(const list_head_t *head)
+{
+    return head->next == head;
+}
+
+#endif
