1e13e54e74b964bf66e223ef5a75407e9c668fca
Tim Yang <tl445047925@gmail.com>
2021-04-08 17:23:34 +0800

    caculate time from count / frequency

--- lab4/peripheral/timer.c
+++ lab4/peripheral/timer.c
@@ -3,8 +3,9 @@
 #include <uart.h>
 #include <printf.h>
 void get_core_time() {
-  size_t time;
-  asm volatile("mrs  %[result], cntpct_el0": [result]"=r"(time));
+  size_t time, count, freq;
+  asm volatile("mrs  %[result], cntpct_el0": [result]"=r"(count));
+  asm volatile("mrs  %[result], cntfrq_el0": [result]"=r"(freq));
+  time = count / freq;
   printf("core timer interrupt: %x\n", time);
-  //asm volatile("mrs  %[result], cntfrq_el0": [result]"=r"(time));
 }
