e63f9b8eff1f7dc0f13ab100c98fe33d83978515
yywu <warren03wu@gmail.com>
2021-04-13 14:50:34 +0800

    lab4 modify exception from ret to eret

--- include/timer.h
+++ include/timer.h
@@ -2,10 +2,19 @@
 #define __TIMER_H__
 
 #include "uart.h"
+#include "system.h"
 #define TIME_FREQ 0x03b9aca0
 
 extern unsigned int get_current_timer_cnt();
 void add_timer(void (*)(char *));
 void timer_print_msg(char *);
+void init_timer_queue();
+
+typedef struct timer_event {
+    unsigned int registerTime, execTime;
+    char msg[20];
+    struct timer_event* next;
+} timer_event;
+
 
 #endif
--- src/timer.c
+++ src/timer.c
@@ -1,1 +1,10 @@
 #include "timer.h"
+
+timer_event timer_event_queue[20];
+
+void init_timer_queue(){
+
+}
+void timer_print_msg(char* msg){
+
+}
