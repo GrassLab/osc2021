d9e704b82fc0c37e5bbec5dd2211957336747bca
bensonlu0120 <63059869+bensonlu0120@users.noreply.github.com>
2021-04-29 05:21:43 +0800

    Add files via upload

---
+++ Lab5/archive/app2/inc/syscall.c
@@ -0,0 +1,67 @@
+#include "syscall.h"
+#include "printf.h"
+
+void delay(int cnt){
+	while(cnt>0)cnt--;
+}
+
+int getpid(){
+	long ret;
+	asm volatile("\
+		svc 1\n\
+		mov %0, x0\n\
+	":"=r"(ret):);
+	return ret;
+}
+
+int uart_read(char* buf,int size){
+	long ret;
+	asm volatile("\
+		svc 2\n\
+		mov %0, x0\n\
+	":"=r"(ret):);
+	return ret;
+}
+
+int uart_write(char* buf,int size){
+	long ret;
+	asm volatile("\
+		svc 3\n\
+		mov %0, x0\n\
+	":"=r"(ret):);
+	return ret;
+}
+
+unsigned int uart_printf(char* fmt,...){
+	char dst[100];
+    //__builtin_va_start(args, fmt): "..." is pointed by args
+    //__builtin_va_arg(args,int): ret=(int)*args;args++;return ret;
+    __builtin_va_list args;
+    __builtin_va_start(args,fmt);
+    unsigned int ret=vsprintf(dst,fmt,args);
+    uart_write(dst,ret);
+    return ret;
+}
+
+int exec(char* name,char** argv){
+	long ret;
+	asm volatile("\
+		svc 4\n\
+		mov %0, x0\n\
+	":"=r"(ret):);
+	return ret;
+}
+
+void exit(){
+	asm volatile("svc 5\n"::);
+	while(1){}
+}
+
+int fork(){
+	long ret;
+	asm volatile("\
+		svc 6\n\
+		mov %0, x0\n\
+	":"=r"(ret):);
+	return ret;
+}
---
+++ Lab5/include/thread.h
@@ -0,0 +1,7 @@
+void threadSchedule();
+int tidGet();
+void exec(char* path,char** argv);
+void exit();
+int fork();
+void threadtest1();
+void threadtest2();
