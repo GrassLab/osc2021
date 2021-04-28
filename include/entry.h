de33a264f3ed3c0bded4669d27ca90b37329aed8
Yunyung <yungyung7654321@gmail.com>
2021-04-28 19:34:18 +0800

    Update: Modify S_FRAME_SIZE 252 to 272(34 regs)

--- include/entry.h
+++ include/entry.h
@@ -2,7 +2,7 @@
 #define _ENTRY_H
  
 
-#define S_FRAME_SIZE            256 		// size of all saved registers 
+#define S_FRAME_SIZE            272 		// size of all saved registers 
 #define S_X0                    0           // offset of x0 register in saved stack frame
 
 #define SYNC_INVALID_EL1t       0 
@@ -25,8 +25,8 @@
 #define FIQ_INVALID_EL0_32      14 
 #define ERROR_INVALID_EL0_32    15 
 
-#define SYNC_ERROR			16 
-#define SYSCALL_ERROR			17 
+#define SYNC_ERROR              16 
+#define SYSCALL_ERROR           17 
 
 #ifndef __ASSEMBLER__
 extern void enable_irq_persist();
