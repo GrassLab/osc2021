ddfbefe21d547a32615f81ad4bd5fda4a7a5fa11
Yunyung <yungyung7654321@gmail.com>
2021-04-28 19:33:15 +0800

    Add two user program for Lab5 - R2 and corresponding initramfs.cpio

--- rootfs/argv_test/src/argv_test.c
+++ rootfs/argv_test/src/argv_test.c
@@ -4,22 +4,17 @@
 
 int main(int argc, char **argv) 
 {
-    // init_printf(0, putc);
+    init_printf(0, putc);
 
-    // printf("\n[exec_argv_test]Argv Test, pid %d\n", call_sys_gitPID());
-    // for (int i = 0; i < argc; ++i) {
-    //     printf("%s\n", argv[i]);
-    // }
+    printf("\n[exec_argv_test]Argv Test, pid %d\n", call_sys_gitPID());
+    for (int i = 0; i < argc; ++i) {
+        printf("%s\n", argv[i]);
+    }
     
-    call_sys_write("help me !\n");
-    call_sys_write("help me !\n");
-    call_sys_write("help me !\n");
-    call_sys_write("help me !\n");
-    call_sys_write("help me !\n");
-    //char *fork_argv[] = {"fork_test", 0};
-    //call_sys_exec((unsigned long)&fork_test, fork_argv);
-    call_sys_write("help me !\n");
-    call_sys_exit();
+    char *fork_argv[] = {"fork_test", 0};
+    call_sys_exec("fork_test", fork_argv);
+
+    call_sys_exit(); // should never run
 
     return 0;
 }
---
+++ rootfs/fork_test/src/fork_test.c
@@ -0,0 +1,28 @@
+#include "printf.h"
+#include "sys.h"
+#include "uart.h"
+
+int main(void) {
+    init_printf(0, putc);
+    
+    printf("\n[fork_test]Fork Test, pid %d\n", call_sys_gitPID());
+    int cnt = 1;
+    int ret = 0;
+    printf("pid: %d, cnt: %d, cnt_adress: 0x%x\n", call_sys_gitPID(), cnt, &cnt);
+    if (call_sys_fork() == 0) { // child
+        printf("pid: %d, cnt: %d, cnt_adress: 0x%x\n", call_sys_gitPID(), cnt, &cnt);
+        ++cnt;
+        call_sys_fork();
+        while (cnt < 5) {
+            printf("pid: %d, cnt: %d, cnt_adress: 0x%x\n", call_sys_gitPID(), cnt, &cnt);
+            for(int i = 0;i < 9999999;i++);
+            ++cnt;
+        }
+    } else {
+        printf("parent here, pid %d, child %d\n", call_sys_gitPID(), ret);
+    }
+    // dumpTasksState();
+    printf("[exit] Task%d\n", call_sys_gitPID());
+    call_sys_exit();
+    return 0;
+}
