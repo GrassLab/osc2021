26ab5a857bec625092792164d2996c547c18b2d2
Black Mamba <73428438+andykuo8766@users.noreply.github.com>
2021-05-01 14:08:42 +0800

    Add files via upload

---
+++ Lab-01/Lab1.si4project/Backup/shell(4648).c
@@ -0,0 +1,95 @@
+
+#include "shell.h"
+#include "string.h"
+#include "command.h"
+#include "uart.h"
+
+void shell_start () 
+{
+    int buffer_counter = 0;
+    char input_char;
+    char buffer[MAX_BUFFER_LEN];
+    enum SPECIAL_CHARACTER input_parse;
+
+    strset (buffer, 0, MAX_BUFFER_LEN);   
+
+    // new line head
+    uart_puts("# ");
+
+    // read input
+    while(1)
+    {
+        input_char = uart_getc();
+
+        
+
+        input_parse = parse ( input_char );
+
+        command_controller ( input_parse, input_char, buffer, &buffer_counter);
+    }
+}
+
+enum SPECIAL_CHARACTER parse ( char c )
+{
+    if ( !(c < 128 && c >= 0) )
+        return UNKNOWN;
+
+    if ( c == BACK_SPACE )
+        return BACK_SPACE;
+    else if ( c == LINE_FEED || c == CARRIAGE_RETURN )
+        return NEW_LINE;
+    else
+        return REGULAR_INPUT;    
+}
+
+void command_controller ( enum SPECIAL_CHARACTER input_parse, char c, char buffer[], int * counter )
+{
+    if ( input_parse == UNKNOWN )
+        return;
+    
+    // Special key
+    if ( input_parse == BACK_SPACE )
+    {
+        if (  (*counter) > 0 )
+            (*counter) --;
+        
+        uart_send(c);
+        uart_send(' ');
+        uart_send(c);
+    }
+    else if ( input_parse == NEW_LINE )
+    {
+        uart_send(c);
+
+        if ( (*counter) == MAX_BUFFER_LEN ) 
+        {
+            input_buffer_overflow_message(buffer);
+        }
+        else 
+        {
+            buffer[(*counter)] = '\0';
+
+            if      ( !strcmp(buffer, "help"        ) ) command_help();
+            else if ( !strcmp(buffer, "hello"       ) ) command_hello();
+            else if ( !strcmp(buffer, "timestamp"   ) ) command_timestamp();
+            else if ( !strcmp(buffer, "reboot"      ) ) command_reboot();
+            else                                        command_not_found(buffer);
+        }
+            
+        (*counter) = 0;
+        strset (buffer, 0, MAX_BUFFER_LEN); 
+
+        // new line head;
+        uart_puts("# ");
+    }
+    else if ( input_parse == REGULAR_INPUT )
+    {
+        uart_send(c);
+
+        if ( *counter < MAX_BUFFER_LEN)
+        {
+            buffer[*counter] = c;
+            (*counter) ++;
+        }
+    }
+}
---
+++ Lab-01/Lab1.si4project/Backup/uart(4003).c
@@ -0,0 +1,112 @@
+#include "gpio.h"
+#include "uart.h"
+
+/**
+ * Set baud rate and characteristics (115200 8N1) and map to GPIO
+ */
+void uart_init()
+{
+    register unsigned int reg;
+
+    /* initialize UART */
+    *AUX_ENABLE     |= 1;       /* enable mini UART */
+    *AUX_MU_CNTL     = 0;       /* Disable transmitter and receiver during configuration. */
+
+    *AUX_MU_IER      = 0;       /* Disable interrupt */
+    *AUX_MU_LCR      = 3;       /* Set the data size to 8 bit. */
+    *AUX_MU_MCR      = 0;       /* Don’t need auto flow control. */
+    *AUX_MU_BAUD     = 270;     /* 115200 baud */
+    *AUX_MU_IIR      = 6;       /* No FIFO */
+    // *AUX_MU_IIR      = 0xc6;       /* No FIFO */
+
+    /* map UART1 to GPIO pins */
+    reg = *GPFSEL1;
+    reg &= ~((7<<12)|(7<<15));  /* address of gpio 14, 15 */
+    reg |=   (2<<12)|(2<<15);   /* set to alt5 */
+
+    *GPFSEL1 = reg;
+
+    *GPPUD = 0;                 /* enable gpio 14 and 15 */
+    reg=150;
+    while ( reg-- )
+    { 
+        asm volatile("nop"); 
+    }
+    
+    *GPPUDCLK0 = (1<<14)|(1<<15);
+    reg=150; 
+    while ( reg-- )
+    {
+        asm volatile("nop");
+    }
+    
+    *GPPUDCLK0 = 0;             /* flush GPIO setup */
+
+    *AUX_MU_CNTL = 3;           // Enable the transmitter and receiver.
+}
+
+/**
+ * Send a character
+ */
+void uart_send(unsigned int c)
+{
+    /* Wait until we can send */
+    do {
+        
+        asm volatile("nop");
+
+    } while( ! ( *AUX_MU_LSR&0x20 ));
+    
+    /* write the character to the buffer */   
+    *AUX_MU_IO = c;
+
+    if ( c == '\n' ) 
+    {
+        do {
+            
+            asm volatile("nop");
+
+        } while( ! ( *AUX_MU_LSR&0x20 ));
+        
+        *AUX_MU_IO = '\r';
+    }
+}
+
+/**
+ * Receive a character
+ */
+char uart_getc() {
+
+    char r;
+    
+    /* wait until something is in the buffer */
+    do{
+        
+        asm volatile("nop");
+        
+    } while ( ! ( *AUX_MU_LSR&0x01 ) );
+
+    /* read it and return */
+    r = ( char )( *AUX_MU_IO );
+
+    /* convert carrige return to newline */
+    return r == '\r' ? '\n' : r;
+}
+
+/**
+ * Display a string
+ */
+void uart_puts(char *s)
+{
+    while( *s )
+    {
+        /* convert newline to carrige return + newline */
+    
+        //if(*s=='\n')
+        //    uart_send('\r');
+
+        uart_send(*s++);
+
+    }
+}
+
