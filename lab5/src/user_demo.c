5dd030cd7c40d3f026b30b6f8f3303b71431f1fd
EricPwg <dz92286@gmail.com>
2021-05-08 01:12:07 +0800

    same as pre commit

--- lab5/src/user_demo.c
+++ lab5/src/user_demo.c
@@ -1,30 +1,25 @@
 # include "user_demo.h"
 # include "user_lib.h"
 
-char *argv_demo[] = {"aaa", "aaaaa", "qqq", "\0", 0};
+# include "uart.h"
 
-void exec_test(int argc, char **argv){
-  char ct[20];
-  uart_write((char *) "argc: ", 5);
-  int_to_str(argc, ct);
-  uart_write(ct, str_len(ct));
-  uart_write((char *) "\n", 1);
-  exit();
-}
+const char *argv_test_argv[] = {"argv_test", "-o", "argv2", "argv3", "Hello~~~", 0};
+const char *fork_test_argv[] = {"fork_test", 0};
 
-void user_demo_test(){
+void fork_test(int argc, char **argv){
+  int cnt = 1;
+  int ret = 0;
   char ct[20];
   uart_write((char *) "pid: ", 5);
   int_to_str(getpid(), ct);
   uart_write(ct, str_len(ct));
+  uart_write((char *) ", cnt: ", 7);
+  int_to_str(cnt, ct);
+  uart_write(ct, str_len(ct));
+  uart_write((char *) ", &cnt: ", 8);
+  ptr_to_hex((void *)(&cnt), ct);
+  uart_write(ct, str_len(ct));
   uart_write((char *) "\n", 1);
-  int cnt = 1;
-  int ret = 0;
-  //
-  //char *argv[] = {"aaa", "aaaaa", "qqq", 0};
-  exec(exec_test, argv_demo);
-  exit();
-  //
   if ((ret = fork()) == 0){
     uart_write((char *) "pid: ", 5);
     int_to_str(getpid(), ct);
@@ -33,7 +28,7 @@ void user_demo_test(){
     int_to_str(cnt, ct);
     uart_write(ct, str_len(ct));
     uart_write((char *) ", &cnt: ", 8);
-    int_to_hex((unsigned long long)(&cnt), ct);
+    ptr_to_hex((void*)(&cnt), ct);
     uart_write(ct, str_len(ct));
     uart_write((char *) "\n", 1);
     cnt++;
@@ -46,10 +41,10 @@ void user_demo_test(){
       int_to_str(cnt, ct);
       uart_write(ct, str_len(ct));
       uart_write((char *) ", &cnt: ", 8);
-      int_to_hex((unsigned long long)(&cnt), ct);
+      ptr_to_hex((void *)(&cnt), ct);
       uart_write(ct, str_len(ct));
       uart_write((char *) "\n", 1);
-      delay(100000000);
+      delay(1000000);
       cnt++;
     }
   }
@@ -61,9 +56,32 @@ void user_demo_test(){
     int_to_str(ret, ct);
     uart_write(ct, str_len(ct));
     uart_write((char *) "\n", 1);
-    char *argv[4] = {"aaa", "aaaaa", "qqq", "\0"};
-    exec(exec_test, argv);
+    delay(10000000);
   }
   exit();
 }
 
+
+void argv_test(int argc, char **argv){
+  char ct[20];
+  uart_write((char *) "Argv Test, pid: ", 16);
+  int_to_str(getpid(), ct);
+  uart_write(ct, str_len(ct));
+  uart_write((char *) "\n", 1);
+  uart_write((char *) "argc: ", 5);
+  int_to_str(argc, ct);
+  uart_write(ct, str_len(ct));
+  uart_write((char *) "\n", 1);
+  for (int i=0; i<argc; i++){
+    uart_write(argv[i], str_len(argv[i]));
+    uart_write((char *)"\n", 1);
+  }
+  exec(fork_test, (char **)fork_test_argv);
+  exit();
+}
+
+
+void user_demo_test(){
+  exec(argv_test, (char **)argv_test_argv);
+  exit();
+}
