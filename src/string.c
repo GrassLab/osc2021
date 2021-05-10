7c6eae601a856655b2cb95f101d5f3d5ba18e395
Yunyung <yungyung7654321@gmail.com>
2021-05-11 03:21:27 +0800

    Add strcpy() to string.c

--- src/string.c
+++ src/string.c
@@ -122,3 +122,13 @@ int strncmp(const char *a, const char *b, unsigned long n)
     }
     return 0;
 }
+
+int strcpy(char *dest, const char *src) {
+    int i = 0;
+    while (src[i] != '\0') {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return i;
+}
