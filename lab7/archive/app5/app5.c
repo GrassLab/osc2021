cec4ced1392f7932ef9b9702b7dca4fba1ecab22
5fe6eb50c7aa19f9 <51sf1hgd@gmail.com>
2021-05-16 16:09:21 +0800

    appearance

--- lab7/archive/app5/app5.c
+++ lab7/archive/app5/app5.c
@@ -1,11 +1,10 @@
 #include "inc/syscall.h"
 
 int main(int argc,char** argv){
-    char buffer[101];
-
+	char buffer[101];
 
 	int fd=open(argv[1],0);
-    uart_printf("read somthing from %s(fd %d):\n",argv[1],fd);
+	uart_printf("read somthing from %s(fd %d):\n",argv[1],fd);
 	while(1){
 		int ret=read(fd,buffer,100);
 		if(ret==0)break;
@@ -17,7 +16,7 @@ int main(int argc,char** argv){
 	fd=open(argv[1],0);
 	uart_printf("\nwrite somthing to %s(fd %d):\n",argv[1],fd);
 	int len=uart_read(buffer,100);
-    write(fd,buffer,len);
+	write(fd,buffer,len);
 	
 	exit();
 }
