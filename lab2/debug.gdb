8bc0c992e8ba237e5dd5df9501f1eb0b3ff8f3c8
Jia-Sen Li <fkl7619480@gmail.com>
2021-05-18 15:35:49 +0800

    Revert "0856123 Lab2"

--- lab2/debug.gdb
+++
@@ -1,2 +0,0 @@
-file build/kernel8.elf
-target remote :1234
--- lab2/lib/utils.h
+++
@@ -1,6 +0,0 @@
-int atoi(char* s);
-void itoa(int num, char *s);
-int ls(char *input, int readfile);
-int strcmp(char *a,char *b);
-void input(char *command);
-void slice_str(char *input, char *output, char *filename, int length);
