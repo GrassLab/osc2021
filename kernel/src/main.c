c23a1c0a1201da61c8c7588b720fa6eddb02f4e9
Chih-Yu Kao <lcd010308@gmail.com>
2021-06-06 16:48:05 +0800

    Add mapping with finer granularity

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,5 +1,6 @@
 #include "alloc.h"
 #include "mini_uart.h"
+#include "mmu.h"
 #include "printf.h"
 #include "shell.h"
 #include "thread.h"
@@ -15,5 +16,17 @@ void main() {
   printf("0x%llx\n", &a);
   int b;
   printf("%p\n", &b);
+
+  uint64_t *pmd = (uint64_t *)PMD_BASE;
+  uint64_t *pte1 = (uint64_t *)(PTE_BASE + 503 * 0x1000);
+  uint64_t *pte2 = (uint64_t *)(PTE_BASE + 504 * 0x1000);
+  printf("0x%llx\n", *((uint64_t *)PGD_BASE));
+  printf("0x%llx\n", *(pmd + 0));
+  printf("0x%llx\n", *(pmd + 1));
+  printf("0x%llx\n", *(pte1 + 510));
+  printf("0x%llx\n", *(pte1 + 511));
+  printf("0x%llx\n", *(pte2 + 0));
+  printf("0x%llx\n", *(pte2 + 1));
+
   run_shell();
 }
