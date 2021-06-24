645831401d43ac8d3129eb3a19417549801d9919
bensonlu0120 <63059869+bensonlu0120@users.noreply.github.com>
2021-06-24 15:31:45 +0800

    Add files via upload

---
+++ Lab8/archive/app5/app5.c
@@ -0,0 +1,22 @@
+#include "inc/syscall.h"
+
+int main(int argc,char** argv){
+	char buffer[101];
+
+	int fd=open(argv[1],0);
+	uart_printf("read somthing from %s(fd %d):\n",argv[1],fd);
+	while(1){
+		int ret=read(fd,buffer,100);
+		if(ret==0)break;
+		buffer[ret]=0;
+		uart_printf("%s",buffer);
+	}
+	close(fd);
+
+	fd=open(argv[1],0);
+	uart_printf("\nwrite somthing to %s(fd %d):\n",argv[1],fd);
+	int len=uart_read(buffer,100);
+	write(fd,buffer,len);
+	
+	exit();
+}
